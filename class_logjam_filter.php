4866545de150c346d3e9e03e306c507a1f953b9b
Te Taipo <hokioi-security@protonmail.ch>
2021-12-13 23:07:18 +1300

    Update class_logjam_filter.php

--- class_logjam_filter.php
+++ class_logjam_filter.php
@@ -5,33 +5,30 @@ class Logjam_Filter {
         # Attempts to exploit CVE-2021-44228
         # Standard
         # - ${jndi:ldap://,${jndi:rmi://,${jndi:ldaps://,${jndi:dns://
-        if ( empty( $user_agent ) ) $user_agent = strtolower( rawurldecode( urldecode( $_SERVER[ 'HTTP_USER_AGENT' ] ) ) );
-        if ( false !== strpos( $user_agent, 'jndi' ) ) {
-            if ( false !== strpos( $user_agent, 'jndi:ldap' ) ||
-                 false !== strpos( $user_agent, 'jndi:rmi' ) ||
-                 false !== strpos( $user_agent, 'jndi:ldaps' ) ||
-                 false !== strpos( $user_agent, 'jndi:dns' ) ) {
+        if ( false !== strpos( $input, 'jndi' ) ) {
+            if ( false !== strpos( $input, 'jndi:ldap' ) ||
+                 false !== strpos( $input, 'jndi:rmip' ) ||
+                 false !== strpos( $input, 'jndi:ldaps' ) ||
+                 false !== strpos( $input, 'jndi:dns' ) ) {
                  return true;
             }
         }
-        # Deal with variants
-        # Examples
-        # - ${${::-j}${::-n}${::-d}${::-i}:${::-l}${::-d}${::-a}${::-p}://${hostname}
-        # - ${jndi:${lower:l}${lower:d}a${lower:p}://[subdomain]${upper:a}[domain]:80/callback}            
-        if ( substr_count( $user_agent, '$' ) >= 4 &&
-             substr_count( $user_agent, '{' ) >= 4 &&
-             substr_count( $user_agent, '}' ) >= 4 &&
-             substr_count( $user_agent, ':' ) >= 4 ) {
-                $list = array( '$', '{', '}', ':', '-' );
-                $flist = str_replace( $list, '', $user_agent );
-                if ( false !== strpos( $flist, 'ldap' ) ||
-                     false !== strpos( $flist, 'rmi' ) ||
-                     false !== strpos( $flist, 'ldaps' ) ||
-                     false !== strpos( $flist, 'dns' ) ) {
-                     return true;
-                }
+        # Deal with variants          
+        if ( substr_count( $input, '$' ) >= 4 &&
+             substr_count( $input, '{' ) >= 4 &&
+             substr_count( $input, '}' ) >= 4 &&
+             substr_count( $input, ':' ) >= 4 ) {
+             $list = array( 'lower', 'upper' );
+             $input = str_replace( $list, '', $input );
+             $input = preg_replace( "/[^a-zA-Z]/i", "", $input );
+             if ( false !== strpos( $input, 'ldap' ) ||
+                  false !== strpos( $input, 'rmip' ) ||
+                  false !== strpos( $input, 'ldaps' ) ||
+                  false !== strpos( $input, 'dns' ) ) {
+                  return true;
+            }
         }
-    return false;
+        return false;
     }
 }
 ?>
